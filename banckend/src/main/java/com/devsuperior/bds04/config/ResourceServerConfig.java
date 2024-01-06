e850a40e4d1ba2bc3343f2ef46427d0260e4503a
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2024-01-06 20:00:16 -0300

    refactor(ResourceServerConfig): changed to ROLE_CLIENT

--- banckend/src/main/java/com/devsuperior/bds04/config/ResourceServerConfig.java
+++ banckend/src/main/java/com/devsuperior/bds04/config/ResourceServerConfig.java
@@ -36,7 +36,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
 	
-	private static final String[] OPERATOR_OR_ADMIN = { "/events/**", "/cities/**" };
+	private static final String[] CLIENT_OR_ADMIN = { "/events/**", "/cities/**" };
 	
 	private static final String[] ADMIN = { "/cities/**" };	
 	
@@ -53,8 +53,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
-		.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
-		.antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
+		.antMatchers(HttpMethod.GET, CLIENT_OR_ADMIN).permitAll()
+		.antMatchers(CLIENT_OR_ADMIN).hasAnyRole("CLIENT", "ADMIN")
 		.antMatchers(ADMIN).hasRole("ADMIN")
 		.anyRequest().authenticated();
 		
