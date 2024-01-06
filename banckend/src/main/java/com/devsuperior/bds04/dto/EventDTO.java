32447508cd3568676285e9e4ff20637083cd00aa
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2024-01-06 18:59:16 -0300

    feat(CityDTO): add NotBlank, FutureOrPresent, NotNull annotation

--- banckend/src/main/java/com/devsuperior/bds04/dto/EventDTO.java
+++ banckend/src/main/java/com/devsuperior/bds04/dto/EventDTO.java
@@ -3,15 +3,23 @@ package com.devsuperior.bds04.dto;
 import java.io.Serializable;
 import java.time.LocalDate;
 
-import com.devsuperior.bds04.entities.Event;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 public class EventDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	
+	@NotBlank(message = "Campo requerido")
 	private String name;
+	
+	@FutureOrPresent(message = "A data do evento não pode ser passada")
 	private LocalDate date;
 	private String url;
+	
+	@NotNull(message = "Campo requerido")
 	private Long cityId;
 	
 	public EventDTO() {
@@ -23,14 +31,6 @@ public class EventDTO implements Serializable {
 		this.date = date;
 		this.url = url;
 		this.cityId = cityId;
-	}
-	
-	public EventDTO(Event entity) {
-		id = entity.getId();
-		name = entity.getName();
-		date = entity.getDate();
-		url = entity.getUrl();
-		cityId = entity.getCity().getId();
 	}
 
 	public Long getId() {
