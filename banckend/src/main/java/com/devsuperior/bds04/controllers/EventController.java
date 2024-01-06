5a971acbcd7265fcadf50a614993528a25c5604f
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2024-01-06 19:58:20 -0300

    feat(EventController): insert method with @PreAuthorize

--- banckend/src/main/java/com/devsuperior/bds04/controllers/EventController.java
+++ banckend/src/main/java/com/devsuperior/bds04/controllers/EventController.java
@@ -1,11 +1,19 @@
 package com.devsuperior.bds04.controllers;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.bds04.dto.EventDTO;
 import com.devsuperior.bds04.services.EventService;
@@ -18,6 +26,14 @@ public class EventController {
 	
 	public EventController(EventService eventService) {
 		this.eventService = eventService;
+	}
+	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
+	@PostMapping
+	public ResponseEntity<EventDTO> insert(@Valid @RequestBody EventDTO dto){
+		dto = eventService.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 	
 	@GetMapping
--- banckend/src/main/java/com/devsuperior/bds04/services/EventService.java
+++ banckend/src/main/java/com/devsuperior/bds04/services/EventService.java
@@ -3,6 +3,7 @@ package com.devsuperior.bds04.services;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds04.dto.EventDTO;
 import com.devsuperior.bds04.entities.Event;
@@ -20,6 +21,15 @@ public class EventService {
 		this.mapper = mapper;
 	}
 	
+	@Transactional
+	public EventDTO insert(EventDTO dto) {
+		Event entity = mapper.toEntity(dto);
+		entity = eventRepository.save(entity);
+		dto = mapper.toDTO(entity);
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
 	public Page<EventDTO> findAll(Pageable pageable) {
 		Page<Event> page = eventRepository.findAll(pageable);
 		return page.map((entity) -> mapper.toDTO(entity));
