875898f86a5f9f850c6d6bb293cbb0bc5075f60b
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2024-01-06 19:57:11 -0300

    feat(CityController): insert method with @PreAuthorize

--- banckend/src/main/java/com/devsuperior/bds04/controllers/CityController.java
+++ banckend/src/main/java/com/devsuperior/bds04/controllers/CityController.java
@@ -1,11 +1,19 @@
 package com.devsuperior.bds04.controllers;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.bds04.dto.CityDTO;
 import com.devsuperior.bds04.services.CityService;
@@ -18,6 +26,14 @@ public class CityController {
 	
 	public CityController(CityService cityService) {
 		this.cityService = cityService;
+	}
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@PostMapping
+	public ResponseEntity<CityDTO> insert(@Valid @RequestBody CityDTO dto){
+		dto = cityService.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 	
 	@GetMapping
--- banckend/src/main/java/com/devsuperior/bds04/services/CityService.java
+++ banckend/src/main/java/com/devsuperior/bds04/services/CityService.java
@@ -3,6 +3,7 @@ package com.devsuperior.bds04.services;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds04.dto.CityDTO;
 import com.devsuperior.bds04.entities.City;
@@ -19,7 +20,16 @@ public class CityService {
 		this.cityRepository = cityRepository;
 		this.mapper = mapper;
 	}
+	
+	@Transactional
+	public CityDTO insert(CityDTO dto) {
+		City entity = mapper.toEntity(dto);
+		entity = cityRepository.save(entity);
+		dto = mapper.toDTO(entity);
+		return dto;
+	}
 
+	@Transactional(readOnly = true)
 	public Page<CityDTO> findAll(Pageable pageable) {
 		Page<City> page = cityRepository.findAll(pageable);
 		return page.map((entity) -> mapper.toDTO(entity));
